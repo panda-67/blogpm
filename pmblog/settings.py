b5abed2f7899c92d1876921978b3e634bc5ac9a4
Lord Arba <samuarrif@gmail.com>
2021-09-03 01:31:21 +0700

    logo full path

--- pmblog/settings.py
+++ pmblog/settings.py
@@ -89,22 +89,22 @@ WSGI_APPLICATION = 'pmblog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    # 'ENGINE': 'django.db.backends.sqlite3',
-    # 'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blogpm',        
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-        'autocommit': True,
-        
-        },
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'blogpm',        
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #     'autocommit': True,
+        
+    #     },
+    # }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
