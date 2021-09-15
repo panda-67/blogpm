5a89317cd45df961a9016ee17a7d6f8e60ffe426
Lord Arba <samuarrif@gmail.com>
2021-09-15 16:38:02 +0700

    fix

--- pmblog/settings.py
+++ pmblog/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blogpm',        
         'USER': 'root',
-        'PASSWORD': 'Panda@!-67',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
