7604b1232334be7b4375936f249a4985523bd215
Lord Arba <samuarrif@gmail.com>
2021-09-19 01:44:38 +0700

    404 desain

--- pmblog/settings.py
+++ pmblog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4r%5@u1u#24&$*oxl+_$jt$gf!ms-*xwve#h+qg4a4#t_$3hx%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
