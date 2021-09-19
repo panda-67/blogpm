71e40cbbc148a9d76d1db771b079c4849138af77
Lord Arba <samuarrif@gmail.com>
2021-09-19 19:19:34 +0700

    500fix

--- pmblog/settings.py
+++ pmblog/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-4r%5@u1u#24&$*oxl+_$jt$gf!ms-*xwve#h+qg4a4#t_$3hx%
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'blogpm.herokuapp.com','127.0.0.1',]
+ALLOWED_HOSTS = ['blogpm.herokuapp.com','127.0.0.1',]
 
 # Application definition
 
