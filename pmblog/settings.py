1e98cb9dd8c372727c89f235b3c896c95df6b210
Lord Arba <samuarrif@gmail.com>
2021-09-25 01:35:17 +0700

    rapikan grid & tags

--- pmblog/settings.py
+++ pmblog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-4r%5@u1u#24&$*oxl+_$jt$gf!ms-*xwve#h+qg4a4#t_$3hx%
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['blogpm.herokuapp.com','127.0.0.1',]
 
